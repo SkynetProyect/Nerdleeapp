14e2c84d4ef8e4af718099a339746b1fe9d9f55a
Lau <laurabedoyacoronado@gmail.com>
2023-09-22 09:28:23 -0500

    commit modelo.py

---
+++ Nerdle/modelo/modelo.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+@dataclass
+class Jugador:
+    nombre_jugador: str = ""
+    correo_jugador: str = ""
+
+
+class Nerdle:
+    def __init__(self):
+        self.nombre_jugador: str = ""
+        self.correo_jugador: str = ""
+
+    def registrar_nombre_jugador(self, nombre_jugador: str) -> str:
+        self.nombre_jugador = nombre_jugador
+        return self.nombre_jugador
+
+    def registrar_correo_jugador(self, correo_jugador: str) -> str:
+        self.correo_jugador = correo_jugador
+        return self.correo_jugador
+
+    @staticmethod
+    def iniciar_juego(self):
+        filas: int = 6
+        columnas: int = 8
+        tablero = [[" " for _ in range(columnas)] for _ in range(filas)]
+        for fila in tablero:
+            print(" | ".join(fila))
+            print("-" * 31)
+        return tablero
